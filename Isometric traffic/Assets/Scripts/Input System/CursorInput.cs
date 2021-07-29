141021c7670a01960b4dcee5b80ad6914e242b1b
kodlon <kodlon.igorchuchman@gmail.com>
2021-07-29 03:12:23 +0300

    Revert "Revert "road turn 90 degrees""
    This reverts commit 69cc32fb0d93ded776627444d6f9d9d3017726df.

---
+++ Isometric traffic/Assets/Scripts/Input System/CursorInput.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class CursorInput : MonoBehaviour
+{
+    private CursorControls controls;
+    private Camera mainCamera;
+
+
+    private void Awake()
+    {
+        controls = new CursorControls();
+        controls.Mouse.LeftClick.started += _ => LMBClicked();
+        //control.Mouse.LeftClick.performed += _ => LMBClicked(); //Called when LMB release
+
+        mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    /// <summary>
+    /// Called when press LMB
+    /// </summary>
+    private void LMBClicked()
+    {
+        DetectObject();
+    }
+
+    private void DetectObject()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(controls.Mouse.MousePosition.ReadValue<Vector2>());
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) & hit.collider != null)
+        {
+            hit.collider.GetComponent<TurnRoad>().RotateTile();
+        }
+    }
+}
