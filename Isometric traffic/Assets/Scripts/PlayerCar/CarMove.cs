e3d35a348242a843ac9a97a582be3935c8d24484
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-04 19:49:50 +0300

    Create method CheckRoad

--- Isometric traffic/Assets/Scripts/PlayerCar/CarMove.cs
+++ Isometric traffic/Assets/Scripts/PlayerCar/CarMove.cs
@@ -2,24 +2,50 @@ using UnityEngine;
 
 public class CarMove : MonoBehaviour
 {
-    [SerializeField] private int carSpeed;
+    [SerializeField] private float startCarSpeed = 3;
+    private float currentCarSpeed;
+    private string roadName;
+
+
+    private void Start()
+    {
+        currentCarSpeed = startCarSpeed;
+    }
 
     private void Update()
     {
+        MoveCar();
+        RoadCheck();
+    }
 
+    private void RoadCheck()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit) & hit.collider != null)
+        {
+            IRoad road = hit.collider.GetComponent<IRoad>();
+            if (road != null & (roadName == null || hit.collider.name != roadName))
+            {
+                currentCarSpeed = road.RoadBehaviour(currentCarSpeed, startCarSpeed, transform);
 
-        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+                roadName = hit.collider.name;
+            }
 
-        GetComponent<Rigidbody>().AddForce(fwd * carSpeed);
+        }
+    }
 
-        if (Physics.Raycast(transform.position, fwd, 2))
-            print("There is something in front of the object!");
+    private void MoveCar()
+    {
+        Vector3 forward = transform.TransformDirection(-Vector3.forward);
+
+        transform.Translate((forward * Time.deltaTime) * currentCarSpeed);
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Vector3 direction = transform.TransformDirection(Vector3.forward) * 2;
+        Vector3 direction = transform.TransformDirection(Vector3.down);
         Gizmos.DrawRay(transform.position, direction);
     }
 }
