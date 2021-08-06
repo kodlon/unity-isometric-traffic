972e744e7230c2340a2da96fcd9b911e7a22f39b
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-06 18:56:54 +0300

    Add FinishRoad script

---
+++ Isometric traffic/Assets/Scripts/Roads/FinishRoad.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class FinishRoad : MonoBehaviour, IRoad
+{
+    public void Click()
+    {
+        return;
+    }
+
+    public float RoadBehaviour(float currentCarSpeed, float startCarSpeed, Transform carTransform)
+    {
+        return 0f;
+    }
+}
