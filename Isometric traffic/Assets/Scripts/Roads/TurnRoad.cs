9adc81afd263dd6fa90cba2a14387259d71af2cc
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-06 18:57:18 +0300

    Add connection check with ray

--- Isometric traffic/Assets/Scripts/Roads/TurnRoad.cs
+++ Isometric traffic/Assets/Scripts/Roads/TurnRoad.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class TurnRoad : MonoBehaviour, IRoad
 {
+    Ray rayRight;
+    Ray rayBack;
+    bool isConnected = false;
+
+
+    private void Update()
+    {
+        CheckRoadOnConnection();
+    }
+
     public void Click()
     {
         this.transform.Rotate(0, -90, 0);
@@ -9,7 +19,39 @@ public class TurnRoad : MonoBehaviour, IRoad
 
     public float RoadBehaviour(float currentCarSpeed, float startCarSpeed, Transform carTransform)
     {
-        carTransform.Rotate(0, -45, 0);
+        carTransform.Rotate(0, -90, 0);
         return startCarSpeed;
+    }
+
+    private void CheckRoadOnConnection()
+    {
+        RaycastHit hitRight;
+        RaycastHit hitBack;
+        if (Physics.Raycast(rayRight, out hitRight) & hitRight.collider != null
+            & Physics.Raycast(rayBack, out hitBack) & hitBack.collider != null)
+        {
+            isConnected = true;
+        }
+        else
+        {
+            isConnected = false;
+        }
+    }
+
+    private void rayInitializer()
+    {
+        rayRight = new Ray(transform.position, transform.TransformDirection(Vector3.right));
+        rayBack = new Ray(transform.position, transform.TransformDirection(Vector3.back));
+    }
+
+    private void OnDrawGizmos()
+    {
+        rayInitializer();
+        if (isConnected)
+            Gizmos.color = Color.green;
+        else
+            Gizmos.color = Color.red;
+        Gizmos.DrawRay(rayRight.origin, rayRight.direction);
+        Gizmos.DrawRay(rayBack.origin, rayBack.direction);
     }
 }
