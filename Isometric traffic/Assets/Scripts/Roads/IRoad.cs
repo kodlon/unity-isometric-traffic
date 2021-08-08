13aad45d93027b5e4a1c3b046d03a12503206609
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-09 01:33:40 +0300

    Add namespace Roads and XML comments

--- Isometric traffic/Assets/Scripts/Roads/IRoad.cs
+++ Isometric traffic/Assets/Scripts/Roads/IRoad.cs
@@ -1,9 +1,22 @@
 ﻿using UnityEngine;
 
-public interface IRoad
+namespace Roads
 {
-    void Click();
+    public interface IRoad
+    {
+        /// <summary>
+        /// Called when mouse clicked on object.
+        /// </summary>
+        void Click();
 
-    float RoadBehaviour(float currentCarSpeed, float startCarSpeed, Transform carTransform);
+        /// <summary>
+        /// The impact of the road on the player's car.
+        /// </summary>
+        /// <param name="currentCarSpeed">Current speed of player car.</param>
+        /// <param name="startCarSpeed">Start speed of player car.</param>
+        /// <param name="carTransform">Player car transform.</param>
+        /// <returns></returns>
+        float RoadBehaviour(float currentCarSpeed, float startCarSpeed, Transform carTransform);
+    }
 }
 
