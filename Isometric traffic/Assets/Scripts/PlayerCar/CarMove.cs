b52654b5e5badf9c229833be8212cdf2dbab037f
Ihor Chuchman <45404949+kodlon@users.noreply.github.com>
2021-08-30 01:20:37 +0300

    Added simple restart, and check no tiles

--- Isometric traffic/Assets/Scripts/PlayerCar/CarMove.cs
+++ Isometric traffic/Assets/Scripts/PlayerCar/CarMove.cs
@@ -9,11 +9,15 @@ namespace PlayerCar
         [SerializeField] private float startCarSpeed = 3;
         private float _currentCarSpeed;
         private string _roadName;
+        private Vector3 _startPosition;
 
 
         private void Start()
         {
+            RestartLevel.OnCount += RestartCar;
+            
             _currentCarSpeed = startCarSpeed;
+            _startPosition = transform.position;
         }
 
         private void Update()
@@ -24,6 +28,7 @@ namespace PlayerCar
                 RoadCheck();
             }
         }
+
 
         private void RoadCheck()
         {
@@ -38,7 +43,10 @@ namespace PlayerCar
 
                     _roadName = hit.collider.name;
                 }
-
+            }
+            else //if no tiles under car
+            {
+                StartLevel.IsLevelStarted = false;
             }
         }
 
@@ -47,13 +55,19 @@ namespace PlayerCar
             transform.Translate(Vector3.back * (Time.deltaTime * _currentCarSpeed));
         }
 
+        private void RestartCar() //TODO: add check rotation, and after finish check 
+        {
+            Debug.Log("Restart");
+            transform.position = _startPosition;
+        }
+
         private void OnDrawGizmos()
         {
             Vector3 position = transform.position;
-        
+
             Vector3 directionDown = transform.TransformDirection(Vector3.down);
             Vector3 directionBack = transform.TransformDirection(Vector3.back);
-        
+
             Gizmos.color = _currentCarSpeed > 0 ? Color.green : Color.red;
             Gizmos.DrawRay(position, directionDown);
             Gizmos.DrawRay(position, directionBack);
